cb6122e465852153aacf7bc0d81a2f8ec25cf6f6
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-05-25 01:06:25 +0530

    update index

--- js/sec1.js
+++ js/sec1.js
@@ -1,14 +1,23 @@
 const buttonContainer = document.getElementById("Sec-1-Btn");
 const button = buttonContainer.getElementsByClassName("sec1Btn");
+const Word = buttonContainer.getElementsByTagName("a");
 button[0].classList.toggle("active");
+Word[0].classList.toggle("WordActive");
 let previousbtn = null;
-for(let i = 0 ; i<button.length;i++){
-    button[i].addEventListener('click',function(){
+let previousWord = null;
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+        Word[0].classList.remove("WordActive");
         button[0].classList.remove("active");
-        if(previousbtn){
+        if (previousbtn) {
             previousbtn.classList.remove("active");
+            previousWord.classList.remove("WordActive");
         }
         this.classList.toggle("active");
-        previousbtn=this;
+        Word[i].classList.toggle("WordActive");
+        previousWord=Word[i];
+        previousbtn = this;
     })
 }
+
+
